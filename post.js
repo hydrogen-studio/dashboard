2ea1caa88fcf815f79ad8386788559286053c6b8
hydrogen-studio <61718408+hydrogen-studio@users.noreply.github.com>
2020-04-06 20:48:51 -0700

    Update post.js

--- post.js
+++ post.js
@@ -1,10 +1,4 @@
-  var x = document.forms["prefixform"]["ownerkey"].value;
-  var y = document.forms["prefixform"]["prefix"].value;
-  
-  xhttp.open("POST", "http://api.hydrogenbot.xyz/v1/ssddpre", true);
-    xhttp.setRequestHeader("ownerkey", x);
-    xhttp.setRequestHeader("prefix", y);
-    xhttp.send("test")
+
   
 
   
