0e1b64b50837ae75afc3793328615989d781b8ab
hydrogen-studio <61718408+hydrogen-studio@users.noreply.github.com>
2020-04-06 20:18:13 -0700

    Update post.php

--- post.php
+++ post.php
@@ -3,4 +3,30 @@
   // To access the data sent with the GET method, you can use $_GET
   $say = htmlspecialchars($_POST['ownerkey']);
   $to  = htmlspecialchars($_POST['prefix']);
+
+$url = "http://api.hydrogenbot.xyz/v1/developer/prefix";
+
+//The data you want to send via POST
+$fields = [
+    'ownerkey'      => $say,
+    'prefix' => $to
+];
+
+//url-ify the data for the POST
+$fields_string = http_build_query($fields);
+
+//open connection
+$ch = curl_init();
+
+//set the url, number of POST vars, POST data
+curl_setopt($ch,CURLOPT_URL, $url);
+curl_setopt($ch,CURLOPT_POST, true);
+curl_setopt($ch,CURLOPT_POSTFIELDS, $fields_string);
+
+//So that curl_exec returns the contents of the cURL; rather than echoing it
+curl_setopt($ch,CURLOPT_RETURNTRANSFER, true); 
+
+//execute post
+$result = curl_exec($ch);
+echo $result;
 ?>
