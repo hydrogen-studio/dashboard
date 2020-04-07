54cdbab72a14eead4751df38aff9f84c0295e385
hydrogen-studio <61718408+hydrogen-studio@users.noreply.github.com>
2020-04-06 21:11:05 -0700

    Delete post.js

--- post.js
+++
@@ -1,6 +0,0 @@
-let Form = document.forms[index];
-let { pre, db, og } = require("../index.js")
-let p = Form.elements["prefix"].value
-let ownk = Form.elements["ownerkey"].value
-  
-pre.set(og.get(ownk).value, { prefix: p })
