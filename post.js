1e5bd2ebd4a67148cea04b9c62258cfd05fb9052
hydrogen-studio <61718408+hydrogen-studio@users.noreply.github.com>
2020-04-06 20:36:12 -0700

    Update post.js

--- post.js
+++ post.js
@@ -1,2 +1,18 @@
-var formData = new FormData();
+  var x = document.forms["prefixform"]["ownerkey"].value;
+  var y = document.forms["prefixform"]["prefix"].value;
+var ok;
+  if (x === "" || y === "") {
+    alert("Both field must be filled out");
+    ok = false
+  }else{
+    ok = true
+  }
 
+  if(ok === true){
+    xhttp.open("POST", "http://api.hydrogenbot.xyz/v1/developer/prefix", true);
+    xhttp.setRequestHeader("ownerkey", x);
+    xhttp.setRequestHeader("prefix", y);
+    xhttp.send()
+  }
+
+  
