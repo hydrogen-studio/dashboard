dc0e71ec7dea42466e4425ae49a697a59eb97b4b
hydrogen-studio <61718408+hydrogen-studio@users.noreply.github.com>
2020-04-06 20:14:09 -0700

    Create post.php

---
+++ post.php
@@ -0,0 +1,6 @@
+<?php
+  // The global $_POST variable allows you to access the data sent with the POST method by name
+  // To access the data sent with the GET method, you can use $_GET
+  $say = htmlspecialchars($_POST['ownerkey']);
+  $to  = htmlspecialchars($_POST['prefix']);
+?>
